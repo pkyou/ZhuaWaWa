9c9f55bc086e13dc713f8fa1a5eaee7c1fc7dbce
pkyou <471162774@qq.com>
2018-01-07 13:19:48 +0800

    增加日志

--- README.md
+++ README.md
@@ -3,3 +3,5 @@ springBoot examples
 # 2017-12-23 16:05:16 add readme.md
 ## add develop branch and feature-pangkang 
 ## down load code 
+# 2018年01月07日 增加swagger ui 整合
+   增加日志到console 和 日志文件
--- src/main/java/com/pkyou/Sample/Controller/HelloWorldController.java
+++ src/main/java/com/pkyou/Sample/Controller/HelloWorldController.java
@@ -1,9 +1,10 @@
 package com.pkyou.Sample.Controller;
 
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
-
+import ch.qos.logback.classic.Logger;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 
@@ -12,10 +13,13 @@ import io.swagger.annotations.ApiOperation;
 @Api("swagger ui 注释 api 级别")
 public class HelloWorldController {
 
+	private final  Logger logger = (Logger) LoggerFactory.getLogger(HelloWorldController.class);
 	@ApiOperation("swagger ui 注释 方法级别")
 	@RequestMapping(value="/hello",method=RequestMethod.GET)
 	public  String hello() {
-		String data = "Never trouble untill trouble troubles you. Some one told me some years ago.";
+		
+		String data = "Never trouble untill.";
+		logger.info("this is debug info");
 		return data;
 	}
 }
