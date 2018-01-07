bd1160f00b148c076a2558ac53c59cf7b9ddf79f
pkyou <471162774@qq.com>
2018-01-07 10:28:35 +0800

    整合swagger ui

--- src/main/java/com/pkyou/Sample/Controller/Main.java
+++ src/main/java/com/pkyou/Sample/Controller/Main.java
@@ -5,7 +5,10 @@ import org.springframework.boot.SpringBootConfiguration;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
 @SpringBootApplication
+@EnableSwagger2
 public class Main {
 
     public static void main(String[] args) throws Exception {
