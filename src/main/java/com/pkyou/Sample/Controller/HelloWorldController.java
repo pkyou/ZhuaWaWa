817b4b50da20cfc6c3db6cb52dba5aae614ede14
pangkang <pangkang@boco.com.cn>
2018-01-08 15:59:31 +0800

    尝试增加mybatis

--- src/main/java/com/pkyou/Sample/Controller/HelloWorldController.java
+++ src/main/java/com/pkyou/Sample/Controller/HelloWorldController.java
@@ -1,25 +1,51 @@
 package com.pkyou.Sample.Controller;
 
+import java.util.ArrayList;
+
+
 import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.pkyou.Sample.Entyties.IndoorCheckItemEntity;
+
 import ch.qos.logback.classic.Logger;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 
 @RestController
-@RequestMapping("/user")
+@RequestMapping("/controller")
 @Api("swagger ui 注释 api 级别")
 public class HelloWorldController {
 
 	private final  Logger logger = (Logger) LoggerFactory.getLogger(HelloWorldController.class);
+	
+	
+	//@Resource
+	//private ControllerService service;
+	
+	
+	
 	@ApiOperation("swagger ui 注释 方法级别")
 	@RequestMapping(value="/hello",method=RequestMethod.GET)
 	public  String hello() {
 		
-		String data = "Never trouble untill.";
-		logger.info("this is debug info");
+		String data = "Never trouble ";
+		logger.info("this");
 		return data;
 	}
+	
+	
+	@RequestMapping(value="/GetIndoorCheckItemEntities",method=RequestMethod.GET)
+	@ApiOperation("获取数据库内容")
+
+	public ArrayList<IndoorCheckItemEntity> GetIndoorCheckItemEntities(){
+	
+		return null;
+		//return service.GetIndoorCheckItemEntities();
+	}
+	
+	
+	
 }
---
+++ src/main/java/com/pkyou/Sample/Entyties/IndoorCheckItemEntity.java
@@ -0,0 +1,33 @@
+package com.pkyou.Sample.Entyties;
+
+import java.io.Serializable;
+
+public class IndoorCheckItemEntity implements Serializable {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -6556793741331167103L;
+	private int id;
+	private int itemId;
+	private String name;
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public int getItemId() {
+		return itemId;
+	}
+	public void setItemId(int itemId) {
+		this.itemId = itemId;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+
+}
