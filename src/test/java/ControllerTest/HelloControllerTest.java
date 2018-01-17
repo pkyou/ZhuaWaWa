d8b046831c392328f517ae12d3d0e7f5b8aad767
pangkang <pangkang@boco.com.cn>
2018-01-17 14:26:32 +0800

    增加controller测试

--- src/test/java/ControllerTest/HelloControllerTest.java
+++ src/test/java/ControllerTest/HelloControllerTest.java
@@ -1,25 +1,43 @@
 package ControllerTest;
 
+import java.net.URL;
+import java.util.ArrayList;
+
+import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.context.embedded.LocalServerPort;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.http.MediaType;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.junit4.SpringRunner;
-import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import com.pkyou.Sample.Main;
+import com.pkyou.Sample.Entyties.IndoorCheckItemEntity;
 
 @RunWith(SpringRunner.class)
-@SpringBootTest
-@AutoConfigureMockMvc
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT,classes = Main.class)
 public class HelloControllerTest {
-	
+
+	@LocalServerPort
+	private int port;
+	private URL base;
 	@Autowired
-	private MockMvc mvc;
+	private TestRestTemplate template;
+
+	@Before
+	public void setUp() throws Exception {
+		this.base = new URL("http://192.168.7.11:" + port + "/controller/GetIndoorCheckItemEntities");
+	}
 
 	@Test
-	public void GetHello() throws Exception{
+	public void getIndoorInfo() throws Exception{
+		ResponseEntity<Object> entities = 
+				template.getForEntity(base.toString(),Object.class);		
+		ArrayList<IndoorCheckItemEntity> body = (ArrayList<IndoorCheckItemEntity>)entities.getBody();
 		
+		Assert.assertNotNull(body);
+		Assert.assertEquals(body.size(), 3);
 	}
 }
