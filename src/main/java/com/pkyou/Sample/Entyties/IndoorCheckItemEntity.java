ca265a8a76c415806a4932379c1afe03ea2f18c4
pkyou <471162774@qq.com>
2020-04-22 13:32:44 +0800

    add MySql database

--- src/main/java/com/pkyou/Sample/Entyties/IndoorCheckItemEntity.java
+++ src/main/java/com/pkyou/Sample/Entyties/IndoorCheckItemEntity.java
@@ -5,24 +5,33 @@ import java.io.Serializable;
 public class IndoorCheckItemEntity implements Serializable {
 	private static final long serialVersionUID = -6556793741331167103L;
 	private int id;
-	private int itemId;
+	private int age;
 	private String name;
+	private String chineseName;
 	public int getId() {
 		return id;
 	}
 	public void setId(int id) {
 		this.id = id;
 	}
-	public int getItemId() {
-		return itemId;
+	public int getAge() {
+		return age;
 	}
-	public void setItemId(int itemId) {
-		this.itemId = itemId;
+	public void setAge(int age) {
+		this.age = age;
 	}
 	public String getName() {
 		return name;
 	}
 	public void setName(String name) {
 		this.name = name;
+	}
+
+	public String getChineseName() {
+		return chineseName;
+	}
+
+	public void setChineseName(String chineseName) {
+		this.chineseName = chineseName;
 	}
 }
