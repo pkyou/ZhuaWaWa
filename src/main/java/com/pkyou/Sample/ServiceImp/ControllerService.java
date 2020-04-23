8080ba60df5a3f1945781ba4c0c3054f1915c8db
pkyou <471162774@qq.com>
2020-04-23 14:48:21 +0800

    read redis data

--- src/main/java/com/pkyou/Sample/ServiceImp/ControllerService.java
+++ src/main/java/com/pkyou/Sample/ServiceImp/ControllerService.java
@@ -3,20 +3,29 @@ package com.pkyou.Sample.ServiceImp;
 import java.util.ArrayList;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
 
 import com.pkyou.Sample.Dao.IIndoorInfoDao;
 import com.pkyou.Sample.Entyties.IndoorCheckItemEntity;
 
- 
-
 @Service
 public class ControllerService   {
-
 	@Autowired
 	private IIndoorInfoDao dao;
+
+	@Autowired
+	private StringRedisTemplate stringRedisTemplate;
 	
 	public ArrayList<IndoorCheckItemEntity> getIndoorCheckItemEntities() {
-		return dao.getWscheckItemsOfIndoor();
+
+		String key1 = stringRedisTemplate.opsForValue().get("key1");
+
+		ArrayList<IndoorCheckItemEntity> wscheckItemsOfIndoor = dao.getWscheckItemsOfIndoor();
+
+		wscheckItemsOfIndoor.get(0).setName(key1);
+		return wscheckItemsOfIndoor;
 	}
 }
--- src/test/java/ControllerTest/HelloControllerTest.java
+++ src/test/java/ControllerTest/HelloControllerTest.java
@@ -8,7 +8,7 @@ import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.context.embedded.LocalServerPort;
+//import org.springframework.boot.context.embedded.LocalServerPort;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.http.ResponseEntity;
@@ -20,7 +20,7 @@ import com.pkyou.Sample.Entyties.IndoorCheckItemEntity;
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT,classes = Main.class)
 public class HelloControllerTest {
 
-	@LocalServerPort
+//	@LocalServerPort
 	private int port;
 	private URL base;
 	@Autowired
